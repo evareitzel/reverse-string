b3d0880defefb235a01a0919ee336f712e6f5e27
Eva Reitzel <evareitzel@gmail.com>
2023-08-15 10:22:20 -0400

    done!

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,6 +1,13 @@
 function reverseString(str) {
   // type your code here
+  let r = ''
+
+  for(let i = str.length-1; i >= 0; i--) {
+    r += str[i]
+  }
+  return r;
 }
+
 
 if (require.main === module) {
   // add your own tests in here
--- ruby/reverse_string.rb
+++ ruby/reverse_string.rb
@@ -1,6 +1,23 @@
 def reverse_string(str)
   # type your code in here
+  
+  split_str = str.split('')
+  # p split_str
+  reversed = []
+  str.size.times { reversed << split_str.pop }
+  reversed.join
 end
+
+
+
+  # JS
+  # let r = ''
+
+  # for(let i = str.length-1; i >= 0; i--) {
+  #   r += str[i]
+  # }
+  # return r
+
 
 if __FILE__ == $PROGRAM_NAME
   puts "Expecting: 'ih'"
